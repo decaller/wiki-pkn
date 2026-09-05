e5ae9d0e8827d9ad2aa386e20a453602146539b2
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2026-09-05 21:09:39 +0700

    feat(branding): update site logo, icon, and favicon with custom pohon fitrah emblem

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -26,6 +26,7 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
     const iconPath = joinSegments(baseDir, "static/icon.png")
+    const appleTouchIcon = joinSegments(baseDir, "static/apple-touch-icon.png")
 
     // Url of current page
     const socialUrl =
@@ -90,6 +91,7 @@ export default (() => {
         )}
 
         <link rel="icon" href={iconPath} />
+        <link rel="apple-touch-icon" href={appleTouchIcon} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
 
